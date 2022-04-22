deddbb403c31b366f6163b54fe10169ae0813901
Julian Villalba <julian_wv@live.com.ar>
2022-04-21 21:56:20 -0300

    add function for drawing and clear canvas

--- js/Index.js
+++ js/Index.js
@@ -6,40 +6,48 @@ function createGrid(divGrid, numbDiv = 256){
     }
 }
 
+function drawingCanvas(){
+    let canvasDivs = document.querySelectorAll('.canvasDiv')
+    let isDrawing = false;
+
+
+    canvas.addEventListener('mousedown', e =>{
+        isDrawing = true;
+    })
+
+
+    canvasDivs.forEach(canvasDivs =>{
+        canvasDivs.addEventListener('mousemove', e =>{
+            if(isDrawing === true){
+                canvasDivs.setAttribute('style', 'background: black');
+            }
+        })
+    })
+
+    window.addEventListener('mouseup', e =>{
+        if(isDrawing === true){
+            isDrawing = false;
+        }
+    })
+}
+
+const clearCanvas = () =>{
+    while(canvas.firstChild){
+        canvas.removeChild(canvas.firstChild);
+    }
+}
+
 let canvas = document.querySelector('#canvas');
 let $hundredPx = document.querySelector('#hundredPx');
 
 createGrid(canvas);
-
-let canvasDivs = document.querySelectorAll('.canvasDiv')
-let isDrawing = false;
-
-
-canvas.addEventListener('mousedown', e =>{
-    isDrawing = true;
-})
-
-
-canvasDivs.forEach(canvasDivs =>{
-    canvasDivs.addEventListener('mousemove', e =>{
-        if(isDrawing === true){
-            canvasDivs.setAttribute('style', 'background: black');
-        }
-    })
-})
-
-window.addEventListener('mouseup', e =>{
-    if(isDrawing === true){
-        isDrawing = false;
-    }
-})
+drawingCanvas();
 
 $hundredPx.addEventListener('click', () =>{
-    while(canvas.firstChild){
-        canvas.removeChild(canvas.firstChild);
-    }
+    clearCanvas();
+    createGrid(canvas, 4096);
+    canvas.style.setProperty('grid-template-columns', 'repeat(64, 1fr)');
 
-    createGrid(canvas, 10000);
-    canvas.style.setProperty('grid-template-columns', 'repeat(100, 1fr)');
+    drawingCanvas();
 
 })
